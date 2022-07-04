feaa38bca2fe089f49d2af2ee76abebc1a3bdacd
Aayush-Gangwar <101112022+Aayush-Gangwar@users.noreply.github.com>
2022-07-04 22:53:09 +0530

    Scripts/box

---
+++ Assets/game scripts/box_opening.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_opening : MonoBehaviour
+{
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)){
+            anim.SetTrigger("open");
+        }
+    }
+}
